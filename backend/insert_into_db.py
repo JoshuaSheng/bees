00ae7669d8bdf1eb16e40948e42ff95cfe281aa5
Joshua Sheng <jsdragonok2@gmail.com>
2021-02-18 23:44:07 -0500

    cleaned up repo, deleted wip frontend in favor of prototype frontend

--- backend/insert_into_db.py
+++ backend/insert_into_db.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
     conn = sql.connect(
         host = "us-cdbr-east-03.cleardb.com",
         user = "b23d8ec954cbba",
-        password = "795b83cc",
+        password = "", #password not given
         database = "heroku_edbb2c5c151f046"
         );
-        
+
     cursor = conn.cursor()
-    
+
     flower_raw = open("flowers")
     flowers = list(flower_raw.read().split(','))
-    
+
 #     try:
 #         for flower in flowers:
 #             sql = "INSERT INTO flowers (flower_name) VALUES ('%s')" % (flower)
